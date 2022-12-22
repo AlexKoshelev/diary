ecd94a0581879cf33bfbc1caf5422654a2bdb780
AlexKoshelev <2391382@mail.ru>
2022-12-22 15:14:47 +0300

    refactoring

---
+++ client/src/app/components/page/diary/previewDiaryPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const PreviewDiaryPage = () => {
+  return <>Войдите чтобы получить доступ</>;
+};
+export default PreviewDiaryPage;
---
+++ client/src/app/components/page/reviewsPage/reviewsPage.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./reviewsPage.scss";
+const ReviewsPage = () => {
+  return <div>ReviewsPage page</div>;
+};
+export default ReviewsPage;
