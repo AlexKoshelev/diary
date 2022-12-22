61877cbeba0091d2cb53524d14e63f32c849ca10
AlexKoshelev <2391382@mail.ru>
2022-12-22 23:18:00 +0300

    server

--- client/src/app/components/ui/comments/comments.jsx
+++ client/src/app/components/ui/comments/comments.jsx
@@ -1,6 +1,6 @@
 import { orderBy } from "lodash";
 import { nanoid } from "nanoid";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   createComment,
